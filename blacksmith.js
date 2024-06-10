68b930ff03b5dc8f4037c94b3011efbf706100f3
Michael Volk <volk0026@algonquinlive.com>
2024-06-09 21:57:56 -0400

    Real version being pushed to Github

--- blacksmith.js
+++ blacksmith.js
@@ -1,5 +1,6 @@
 // Variables
-
+const fireStatus = false;
+const wood = 3
 
 
 // Functions
@@ -12,27 +13,124 @@
  * To stop a fire:
  *    The fire must be going
  */
+function fire () {
+  if (fireStatus === false && wood > 0){
+    fireStatus = true
+    wood = wood - 1
+    console.log('You have started a fire.')
+  }
+  else if (fireStatus === true) {
+    console.log('You have put out the fire')
+    fireStatus = false
+  }
+  else {
+    console.log('You do not have enough wood to start a fire')
+  }
+}
 
 /**
  * buy
  */
 
+function buy(item){
+  if (item === 'ore' && gold >= 3 && fireStatus === false){
+    ore = ore + 1
+    gold = gold - 3
+    console.log('You bought 1 ore')
+  }
+  if  (item === 'wood'){
+wood = wood + 1
+gold = gold - 1
+console.log('You bought 1 wood')
+}
+}
+
 
 /**
  * make
  */
-
+function make(item){
+  const recipies = { sword: { ore: 2, wood: 1 }, axe: { ore: 1, wood: 2 } }
+  if (item === 'sword') {
+    ore = ore - 2
+    wood = wood -1
+    console.log('You have made 1 sword')
+  }
+  if (item ==='axe') {
+    ore = ore - 1
+    wood = wood - 2
+    console.log('You have made 1 axe')
+  }
+  else {
+    return "Invalid item.";
+  }
+}
 
 /**
  * sell
  */
+function sell(item){
+  const prices = { sword: 5, axe: 4 };
+  if (item === 'sword') {
+    gold = gold + 5
+    console.log('You have sold 1 sword')
+  }
+  if (item === 'axe') {
+    gold = gold + 4
+    console.log('You have now sold 1 axe')
+  }
+}
 
 
 /**
  * inventory
  * Shows the players current inventory
  */
+function inventory (){
+  console.log('Current Inventory: Gold: X, Ore: X, Wood: X, Swords: X, Axes: X')
+  return `Inventory:
+  Gold: ${inventory.gold}
+  Ore: ${inventory.ore}
+  Wood: ${inventory.wood}
+  Swords: ${inventory.sword}
+  Axes: ${inventory.axe}`;
+}
 
+
+
+function executeCommand() {
+  const commandInput = document.getElementById("commandInput");
+  const outputElement = document.getElementById("output");
+
+  const command = commandInput.value.toLowerCase();
+
+  let output;
+  switch (command) {
+    case "buy ore":
+    case "buy wood":
+      output = buy(command.split(' ')[1]);
+      break;
+    case "make sword":
+    case "make axe":
+      output = make(command.split(' ')[1]);
+      break;
+    case "sell sword":
+    case "sell axe":
+      output = sell(command.split(' ')[1]);
+      break;
+    case "fire":
+      output = fire();
+      break;
+    case "inventory":
+      output = displayInventory();
+      break;
+    case "help":
+      output = help();
+      break;
+    default:
+      output = "Invalid command. Type 'help' for instructions.";
+  }
+}
 
 /**
  * Help Command
